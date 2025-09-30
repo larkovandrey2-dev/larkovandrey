b68b5f862cc3e94b84e44342314b17616a1b44ac
Vladislav Yashnov <vayashnov@mai.education>
2025-09-30 18:26:52 +0300

    new database methods

--- Mireya/database_scripts.py
+++ Mireya/database_scripts.py
@@ -8,22 +8,24 @@ SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY  = os.getenv('SUPABASE_KEY')
 SUPABASE_SERVICE_KEY = os.getenv('SUPABASE_SERVICE_KEY')
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
-#Добавить функцию изменения статов пользователя, чтобы и последний номер опроса можно было менять и количество пройденных опросов
-#Добавить строки пол, возраст, образование в пользователи, таблица вопросов:текст вопроса, принадлежность к опросу(1,2,...). Метод изменения вопросов и их добавления
+
 def all_users() -> list:
+    '''returns a list with every user_id'''
     response = supabase.table("users").select("user_id").execute()
     users = [item["user_id"] for item in response.data]
     return users
-def create_user(id :int, last_survey_index: int, role = "user", refer_id = 0):
+
+
+def create_user(user_id :int, role: str, refer_id: int):
+    '''create user'''
     try:
-        if id in all_users():
+        if user_id in all_users():
             print("User already exists")
         else:
             new_user = {
-                "user_id": id,
+                "user_id": user_id,
                 "role": role,
                 "refer_id": refer_id,
-                "last_survey_index": last_survey_index # default None
             }
             response = supabase.table('users').insert(new_user).execute()
             if response:
@@ -31,17 +33,54 @@ def create_user(id :int, last_survey_index: int, role = "user", refer_id = 0):
     except Exception as e:
         print(f"Error in create_user: {e}")
 
-def get_user_stats(id: int) -> dict:
+
+def delete_user(user_id: int):
+    '''delete user by user_id'''
     try:
-        id = int(id)
-        if id not in all_users():
+        response = supabase.table("users").delete().eq("user_id", user_id).execute()
+    except Exception as e:
+        print(f"Error in delete_question: {e}")
+
+
+def get_user_stats(user_id: int) -> dict:
+    '''returns a dict of all stats'''
+    try:
+        user_id = int(user_id)
+        if user_id not in all_users():
             print("No such user, creating...")
-            create_user(id)
-        response = supabase.table("users").select("role, refer_id, surveys_count, last_survey_index").eq("user_id", id).execute()
+            create_user(user_id)
+        response = supabase.table("users").select("role, refer_id, surveys_count, last_survey_index, sex, age, education").eq("user_id", user_id).execute()
         user_data = response.data[0]
         return user_data
     except Exception as e:
         print(f"Error in get_user_stats: {e}")
+
+
+def change_user_stat(user_id: int, stat_name: str, new_value):
+    '''change one specific stat by its name and its new value'''
+    try:
+        new_response = {f"{stat_name}": new_value}
+        response = supabase.table("users").update(new_response).eq("user_id", user_id).execute()
+    except Exception as e:
+        print(f"Error in change_user_stat: {e}")
+
+
+def change_user_stats(user_id: int, role: str, refer_id: int, surveys_count: int, last_survey_index: int, sex: str, age: int, education: str):
+    '''change all stats for given user_id (function waits for every stat to be given)'''
+    try:
+        new_response = {
+                    "role": role,
+                    "refer_id": refer_id,
+                    "last_survey_index": last_survey_index,
+                    "surveys_count": surveys_count,
+                    "sex": sex,
+                    "age": age,
+                    "education": education
+                }
+        response = supabase.table("users").update(new_response).eq("user_id", user_id).execute()
+    except Exception as e:
+        print(f"Error in change_user_stats: {e}")
+
 
 def add_gad7_answer(user_id: int, survey_index: int, question_index: int, response_text: str, response_date: str):
     try:
@@ -55,3 +94,45 @@ def add_gad7_answer(user_id: int, survey_index: int, question_index: int, respon
         response = supabase.table("user_gad7_responses").insert(new_response).execute()
     except Exception as e:
         print(f"Error in add_gad7_answer: {e}")
+
+
+def all_questions():
+    '''returns list of dicts: {'question_index':, 'survey_index':, 'question_text':}'''
+    try:
+        response = supabase.table("all_questions").select("question_index, survey_index, question_text").execute()
+        return response.data
+    except Exception as e:
+        print(f"Error in all_questions: {e}")
+
+
+def add_question(question_index: int, survey_index: int, question_text: str):
+    try:
+        new_response = {
+            "survey_index": int(survey_index),
+            "question_index": int(question_index),
+            "question_text": str(question_text)
+        }
+
+        if new_response in all_questions():
+            print("This question exists")
+        else:
+            response = supabase.table("all_questions").insert(new_response).execute()
+    except Exception as e:
+        print(f"Error in add_gad7_answer: {e}")
+
+
+def change_question(question_index: int, survey_index: int, new_question_text: str):
+    '''change question text by its question_index and survey_index'''
+    try:
+        new_response = {"question_text": new_question_text}
+        response = supabase.table("all_questions").update(new_response).eq("question_index", question_index).eq("survey_index", survey_index).execute()
+    except Exception as e:
+        print(f"Error in change_question: {e}")
+
+
+def delete_question(question_index: int, survey_index: int):
+    '''delete question by its question_index and survey_index'''
+    try:
+        response = supabase.table("all_questions").delete().eq("question_index", question_index).eq("survey_index", survey_index).execute()
+    except Exception as e:
+        print(f"Error in delete_question: {e}")
