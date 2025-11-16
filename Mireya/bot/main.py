d88d7293fcb490708e63d3bdc4fff578c3ee8599
larkovandrey2-dev <larkovandrey2@gmail.com>
2025-11-16 13:44:22 +0300

    Docker-test

--- Mireya/bot/main.py
+++ Mireya/bot/main.py
@@ -1,19 +1,43 @@
 import asyncio
+import logging
+from contextlib import asynccontextmanager
+
 from aiogram import Bot, Dispatcher
-from bot.config import BOT_TOKEN
+from aiogram.types import Update
+
+from bot.config import BOT_TOKEN,WEBHOOK_PATH,WEBHOOK_URL
 from bot.handlers import user,admin,survey,start, llm_talk
+from fastapi import FastAPI, Request
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-
+dp.include_router(start.router)
 dp.include_router(user.router)
 dp.include_router(admin.router)
 dp.include_router(survey.router)
-dp.include_router(start.router)
 dp.include_router(llm_talk.router)
 
-async def main():
-    await dp.start_polling(bot,skip_updates=True)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await bot.set_webhook(WEBHOOK_URL)
+    print('Webhook set: ', WEBHOOK_URL)
+    yield
+    await bot.delete_webhook()
+    print('Webhook deleted')
+app = FastAPI(lifespan=lifespan)
+@app.post(WEBHOOK_PATH)
+async def webhook(update: dict):
+    tg_update = Update.model_validate(update)
+    await dp.feed_update(bot, tg_update)
+    return {'ok': True}
+
+def main():
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080,reload=False)
+
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
